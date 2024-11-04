f9535908921ce2fc23e7ce5886241c9274ed1442
Moreira_Stephane <moreira.s@outlook.com>
2024-11-04 09:45:17 +0100

    Start jour05

---
+++ jour03/job01/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.7.1.slim.min.js" integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=" crossorigin="anonymous"></script>
+    <script src="script.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <p>“Les logiciels et les cathédrales, c'est un peu la même chose - d'abord on les construit,
+    ensuite on prie.”</p>
+    <button>CLIQUE</button>
+</body>
+</html>
