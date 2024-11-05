8434f23d35ab9abfd880a8c3975b457018927104
Moreira_Stephane <moreira.s@outlook.com>
2024-11-05 11:45:11 +0100

    Permier input fonctionnel

--- jour05/index.php
+++ jour05/index.php
@@ -17,25 +17,27 @@
 
     <main>
         <form action="" method="post">
-            <label class="label-inscription" for="email">E-Mail*:</label>
-            <input class="input-inscription" type="text" id="email" name="email" placeholder="Adresse e-mail valide" required>
-            <span id="emailError"></span>
-            
-            <label class="label-inscription" for="pass1">Mot de Passe*:</label>
+            <label class="label-inscription" for="lastname">Nom*:</label>
+            <input class="input-inscription" type="text" id="lastname" name="lastname" placeholder="Votre nom" value="" required>
+            <span id="lastnameError"></span>
+
+            <label class="label-inscription" for="firstname">Prénom*:</label>
+            <input class="input-inscription" type="text" id="firstname" name="firstname" placeholder="Votre prenom" value="">
+            <span id="firstnameError"></span>
+
+            <!-- <label class="label-inscription" for="email">E-Mail*:</label>
+            <input class="input-inscription" type="email" id="email" name="email" placeholder="Adresse e-mail valide" required>
+            <span id="emailError"></span> -->
+
+            <!-- <label class="label-inscription" for="pass1">Mot de Passe*:</label>
             <input class="input-inscription" type="password" id="pass1" name="pass1" placeholder="1min/1MAJ/1chiffre/carac. spécial/ 8 carac mini" required>
             <span id="pass1Error"></span>
 
             <label class="label-inscription" for="pass2">Confirmer le mot de passe*:</label>
             <input class="input-inscription" type="password" id="pass2" name="pass2"required>
-            <span id="pass2Error"></span>
+            <span id="pass2Error"></span>-->
 
-            <label class="label-inscription" for="lastname">Nom*:</label>
-            <input class="input-inscription" type="text" id="lastname" name="lastname" placeholder="Votre nom" value="" required>
-            <span id="firstnameError"></span>
 
-            <label class="label-inscription" for="firstname">Prénom*:</label>
-            <input class="input-inscription" type="text" id="firstname" name="firstname" placeholder="Votre prenom" value="">
-            <span id="lastnameError"></span>
 
             <input class="btn-bleu" type="submit" name="submit-btn" value="Inscription">
 
